b25198862ee82fd441143a6aa805af8eeaf8e1d3
j2jon <85131132+j2jon@users.noreply.github.com>
2025-06-27 13:14:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ container.addEventListener("scroll", () => {
   const progressWidth = (scrollLeft / scrollWidth) * 100;
   progress.style.width = `${progressWidth}%`;
 });
+
+var typed = new Typed('#typed', {
+  strings: ['CYBERSECURITY INNOVATOR', 'FULL STACK SOFTWARE ENGINEER', 'DATA SCIENTIST', 'REVERSE ENGINEER', 'MENTOR &amp; TECHNOLOGIST'],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 1500,
+  loop: true
+});
+  
+VANTA.GLOBE({
+el: "#vanta",
+mouseControls: true,
+touchControls: true,
+minHeight: 200.00,
+minWidth: 200.00,
+scale: 1.0,
+scaleMobile: 1.0,
+color: 0xfef08a,
+backgroundColor: 0x0f0f0f
+});
