ca0b7a48ae2fa226ba7bd5497ac303204a135abf
j2jon <85131132+j2jon@users.noreply.github.com>
2025-06-27 13:21:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const container = document.querySelector(".timeline-container");
-const progress = document.querySelector(".timeline-progress");
-container.addEventListener("scroll", () => {
-  const scrollWidth = container.scrollWidth - container.clientWidth;
-  const scrollLeft = container.scrollLeft;
-  const progressWidth = (scrollLeft / scrollWidth) * 100;
-  progress.style.width = `${progressWidth}%`;
-});
-
 var typed = new Typed('#typed', {
   strings: ['CYBERSECURITY INNOVATOR', 'FULL STACK SOFTWARE ENGINEER', 'DATA SCIENTIST', 'REVERSE ENGINEER', 'MENTOR &amp; TECHNOLOGIST'],
   typeSpeed: 60,
@@ -16,13 +7,52 @@ var typed = new Typed('#typed', {
 });
   
 VANTA.GLOBE({
-el: "#vanta",
-mouseControls: true,
-touchControls: true,
-minHeight: 200.00,
-minWidth: 200.00,
-scale: 1.0,
-scaleMobile: 1.0,
-color: 0xfef08a,
-backgroundColor: 0x0f0f0f
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xfef08a,
+  backgroundColor: 0x0f0f0f
+});
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".navbar-right a");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const id = entry.target.getAttribute("id");
+    const navLink = document.querySelector(`.navbar-right a[href="#${id}"]`);
+
+    if (entry.isIntersecting) {
+      navLinks.forEach(link => link.classList.remove("active"));
+      navLink.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.6
+});
+
+sections.forEach(section => observer.observe(section));
+
+
+const floatingLinks = document.querySelectorAll(".dot-link");
+function activateLinkOnScroll() {
+  let index = sections.length;
+  while (--index && window.scrollY + 200 < sections[index].offsetTop) {}
+  floatingLinks.forEach((link) => link.classList.remove("active"));
+  floatingLinks[index]?.classList.add("active");
+}
+activateLinkOnScroll();
+window.addEventListener("scroll", activateLinkOnScroll);
+
+const container = document.querySelector(".timeline-container");
+const progress = document.querySelector(".timeline-progress");
+container.addEventListener("scroll", () => {
+  const scrollWidth = container.scrollWidth - container.clientWidth;
+  const scrollLeft = container.scrollLeft;
+  const progressWidth = (scrollLeft / scrollWidth) * 100;
+  progress.style.width = `${progressWidth}%`;
 });
