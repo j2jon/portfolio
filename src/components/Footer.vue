65beee03741862ce2b9159912b357c1a3ff1314b
j2jon <85131132+j2jon@users.noreply.github.com>
2025-06-28 18:45:06 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,25 +8,8 @@
     <section
       class="flex flex-col gap-4 2xl:flex-row 2xl:items-center 2xl:justify-between 2xl:px-40"
     >
-      <div class="flex flex-col gap-4">
-        <MainText :animated="false" class="2xl:!text-responsive-h2" />
-        <div>Powered by Phoexa</div>
-        <div>
-          Designed by
-          <a
-            class="text-secondary underline"
-            href="https://contra.com/falzo_creations_612dk8w7"
-            target="_blank"
-          >
-            Falzo
-          </a>
-        </div>
-      </div>
-
+      
       <div class="flex gap-4 2xl:translate-y-1/2 2xl:flex-col">
-        <a href="https://github.com/" target="_blank">
-          <img src="/icons/github.png" alt="github" />
-        </a>
         <a href="https://www.linkedin.com/in/jonathan-johnson-87b799171/" target="_blank">
           <img src="/icons/linkedin.png" alt="linkedin" />
         </a>
