52a3f01f59b53fc96f79ba017aa17fb81780d80a
j2jon <85131132+j2jon@users.noreply.github.com>
2025-06-28 00:11:47 -0500

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,8 +5,9 @@
     <div class="flex flex-col gap-8 pl-4 2xl:w-[90%] 2xl:flex-row 2xl:gap-60 2xl:pl-8 [&>*]:flex-1">
       <div class="flex flex-col gap-8">
         <div>
-          <p>Software engineer with over 7 years of experience.</p>
-          <p>Part time wizard & proud bug developer.</p>
+          <p>Technical Expert</p>
+          <p>Passionate Mentor</p>
+          <p>Lifelong Learner</p>
         </div>
 
         <div class="hidden gap-2 text-secondary 2xl:flex">
@@ -16,14 +17,11 @@
       </div>
 
       <p class="mb-8 2xl:mb-0">
-        Passionate about computer science since childhood, I've spent over 7 years refining my
-        skills through diverse projects.
+        Technical expert that's passionate about building smart solutions and mentoring young adults.
         <br />
-        Ever since I was 9 years old, I've loved computer science. This interest has grown into a
-        full career where I keep learning and staying up-to-date with technology.
+        I combine hands-on experience in tech with a commitment to guiding others through career advice, skill building, and personal growth.
         <br />
-        I like to learn by doing, not watching tutorials. Over the years, I have independently
-        developed numerous open-source projects to experiment with a wide range of technologies
+        My mission is to empower others through mentorship, sharing knowledge, and continuous learning.
       </p>
     </div>
     <div class="ml-4 flex gap-2 text-secondary 2xl:hidden">
